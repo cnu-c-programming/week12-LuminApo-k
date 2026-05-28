b849eff22564ec3c257f733d4070ecab1f912f0d
LuminApo-k <beomseomoon@gmail.com>
2026-05-28 20:15:48 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -3,25 +3,52 @@
 #include <stdlib.h>
 
 typedef struct config {
-    char InputFileName[64];
+    char InputFilename[64];
     int Options;
     char SectionName[64];
     unsigned long long Address;
 } Config;
 
-void config_parser(Config* config_ptr) {
+void config_parser(Config *config) {
+    FILE *fp = fopen("config.txt", "r");
+    if (fp == NULL) {
+        return;
+    }
 
+    char line[256];
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        char key[64];
+        char value[128];
+
+        if (sscanf(line, "%63[^=]=%127s", key, value) != 2) {
+            continue;
+        }
+
+        if (strcmp(key, "InputFilename") == 0) {
+            strcpy(config->InputFilename, value);
+        } else if (strcmp(key, "Options") == 0) {
+            config->Options = atoi(value);
+        } else if (strcmp(key, "SectionName") == 0) {
+            strcpy(config->SectionName, value);
+        } else if (strcmp(key, "Address") == 0) {
+            config->Address = strtoull(value, NULL, 10);
+        }
+    }
+
+    fclose(fp);
 }
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[]) {
     Config config;
+
     config_parser(&config);
 
-    printf("config: %s %d %s %llu\n", 
-        config.InputFileName, 
-        config.Options,
-        config.SectionName,
-        config.Address);
+    printf("config: %s %d %s %llu\n",
+           config.InputFilename,
+           config.Options,
+           config.SectionName,
+           config.Address);
+
     return 0;
 }
-
