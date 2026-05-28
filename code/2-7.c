6094cf44bacafbc479c9a75d70762dff8d0b808e
LuminApo-k <beomseomoon@gmail.com>
2026-05-28 22:16:56 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -1,38 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef struct config {
-    char InputFilename[64];
+    char InputFileName[64];
     int Options;
     char SectionName[64];
     unsigned long long Address;
 } Config;
 
-void config_parser(Config *config) {
-    FILE *fp = fopen("config.txt", "r");
-    if (fp == NULL) {
-        return;
+void trim(char *str) {
+    int len = strlen(str);
+
+    while (len > 0 && isspace((unsigned char)str[len - 1])) {
+        str[len - 1] = '\0';
+        len--;
     }
 
+    int start = 0;
+    while (str[start] != '\0' && isspace((unsigned char)str[start])) {
+        start++;
+    }
+
+    if (start > 0) {
+        memmove(str, str + start, strlen(str + start) + 1);
+    }
+}
+
+void config_parser(Config *config) {
+    FILE *fp = fopen("config.txt", "r");
     char line[256];
 
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        char key[64];
-        char value[128];
+    config->InputFileName[0] = '\0';
+    config->Options = 0;
+    config->SectionName[0] = '\0';
+    config->Address = 0;
 
-        if (sscanf(line, "%63[^=]=%127s", key, value) != 2) {
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        char *equal = strchr(line, '=');
+
+        if (equal == NULL) {
             continue;
         }
 
-        if (strcmp(key, "InputFilename") == 0) {
-            strcpy(config->InputFilename, value);
+        *equal = '\0';
+
+        char key[64];
+        char value[128];
+
+        strcpy(key, line);
+        strcpy(value, equal + 1);
+
+        trim(key);
+        trim(value);
+
+        if (strcmp(key, "InputFileName") == 0 || strcmp(key, "InputFilename") == 0) {
+            strcpy(config->InputFileName, value);
         } else if (strcmp(key, "Options") == 0) {
             config->Options = atoi(value);
         } else if (strcmp(key, "SectionName") == 0) {
             strcpy(config->SectionName, value);
         } else if (strcmp(key, "Address") == 0) {
-            config->Address = strtoull(value, NULL, 10);
+            config->Address = strtoull(value, NULL, 0);
         }
     }
 
@@ -45,7 +75,7 @@ int main(int argc, const char *argv[]) {
     config_parser(&config);
 
     printf("config: %s %d %s %llu\n",
-           config.InputFilename,
+           config.InputFileName,
            config.Options,
            config.SectionName,
            config.Address);
