a11eb58b5889f4db4d957d3bbb52b881d23ff26c
LuminApo-k <beomseomoon@gmail.com>
2026-05-28 22:19:15 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(int argc, const char* argv[]) {
-    const char* filename = "numbers.txt";
+int main(int argc, const char *argv[]) {
+    const char *filename = "numbers.txt";
 
-    FILE* fp = fopen(filename, "r");
+    FILE *fp = fopen(filename, "r");
     if (fp == NULL) {
         return 0;
     }
 
-
+    char line[256];
     int sum = 0;
 
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        line[strcspn(line, "\n")] = '\0';
+
+        char *endptr;
+        long value = strtol(line, &endptr, 10);
+
+        while (isspace((unsigned char)*endptr)) {
+            endptr++;
+        }
+
+        if (line[0] == '\0' || *endptr != '\0') {
+            fprintf(stderr, "invalid input %s\n", line);
+        } else {
+            sum += value;
+        }
+    }
 
     printf("sum: %d\n", sum);
+
     fclose(fp);
+
     return 0;
 }
